12105e4c5d845a830a03032c9a570ac018e33de3
Nguyen Nhat Trung <ngnhtrg@phystech.edu>
2024-05-24 19:09:40 +0300

    Add urls

---
+++ src/data/Urls.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://support.mollywlove.ru/api';
+const CART_URL = 'https://support.mollywlove.ru/cart/api';
+// const BASE_URL = 'http://127.0.0.1:8000/api';
+// const CART_URL = 'http://127.0.0.1:8000/cart/api';
+export { BASE_URL, CART_URL };
